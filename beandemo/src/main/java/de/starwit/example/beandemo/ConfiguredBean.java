1ce40ba1e6c4e0fe3b9b9d4d6cf115fd5a304b70
ztarbug <kronprinzfriedrich@gmx.de>
2023-08-28 11:35:01 +0200

    add simple validation example

--- beandemo/src/main/java/de/starwit/example/beandemo/ConfiguredBean.java
+++ beandemo/src/main/java/de/starwit/example/beandemo/ConfiguredBean.java
@@ -3,12 +3,49 @@ package de.starwit.example.beandemo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+
 
 public class ConfiguredBean {
     
     private Logger log = LoggerFactory.getLogger(ConfiguredBean.class); 
 
+    @Email
+    private String email;
+
+    @NotBlank
+    private String aStructuredString;
+
+    @Max(value = 10)
+    private int aNumber;
+
     public void init() {
-        log.info("Configured Bean is about to initialized");
+        log.info("Configured Bean is about to be initialized");
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getaStructuredString() {
+        return aStructuredString;
+    }
+
+    public void setaStructuredString(String aStructuredString) {
+        this.aStructuredString = aStructuredString;
+    }
+
+    public int getaNumber() {
+        return aNumber;
+    }
+
+    public void setaNumber(int aNumber) {
+        this.aNumber = aNumber;
     }
 }
--- beandemo/src/main/java/de/starwit/example/beandemo/SimpleComponent.java
+++ beandemo/src/main/java/de/starwit/example/beandemo/SimpleComponent.java
@@ -1,16 +1,25 @@
 package de.starwit.example.beandemo;
 
+import java.util.Set;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 import jakarta.annotation.PostConstruct;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+
 
 @Component
+@Validated
 public class SimpleComponent {
     
-    private Logger log = LoggerFactory.getLogger(SimpleComponent.class);  
+    private Logger log = LoggerFactory.getLogger(SimpleComponent.class); 
 
     @Autowired
     SimpleBean sBean;
@@ -22,5 +31,19 @@ public class SimpleComponent {
     public void doStuffOnStartup() {
         log.info("Bean constructed");
         sBean.doSomething();
+
+        cBean.setEmail("nomail");
+        cBean.setaNumber(11);
+        cBean.setaStructuredString("");
+
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        Set<ConstraintViolation<ConfiguredBean>> violations = validator.validate(cBean);
+        if (!violations.isEmpty()) {
+            //throw new ConstraintViolationException(violations);
+            for (ConstraintViolation<ConfiguredBean> violation : violations) {
+                log.info(violation.getMessage());
+            }
+        }
     }
 }
