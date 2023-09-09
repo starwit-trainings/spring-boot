d9602838eb9c9f12817064b69dc56356247612e2
witchpou <anett_huebner@gmx.de>
2023-09-09 13:55:07 +0200

    added keycloak and tests

--- usermanagement/persistence/src/main/java/de/user/persistence/repository/OrganisationRepository.java
+++ usermanagement/persistence/src/main/java/de/user/persistence/repository/OrganisationRepository.java
@@ -1,15 +1,15 @@
 package de.user.persistence.repository;
 
 import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
+
 import de.user.persistence.entity.OrganisationEntity;
 
 /**
  * Organisation Repository class
  */
-@Repository
 public interface OrganisationRepository extends JpaRepository<OrganisationEntity, Long> {
 
     @Query("SELECT e FROM OrganisationEntity e WHERE NOT EXISTS (SELECT r FROM e.address r)")
@@ -17,4 +17,6 @@ public interface OrganisationRepository extends JpaRepository<OrganisationEntity
 
     @Query("SELECT e FROM OrganisationEntity e WHERE NOT EXISTS (SELECT r FROM e.address r WHERE r.id <> ?1)")
     public List<OrganisationEntity> findAllWithoutOtherAddress(Long id);
+
+    public List<OrganisationEntity> findByName(String name);
 }
--- usermanagement/persistence/src/test/java/de/user/persistence/repository/OrganisationRepositoryTest.java
+++ usermanagement/persistence/src/test/java/de/user/persistence/repository/OrganisationRepositoryTest.java
@@ -1,5 +1,6 @@
 package de.user.persistence.repository;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import de.user.persistence.entity.OrganisationEntity;
@@ -12,6 +13,24 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 /**
  * Tests for OrganisationRepository
+ * Annotation for a JPA test that focuses only on JPA components.
+ * Using this annotation will disable full auto-configuration and
+ * instead apply only configuration relevant to JPA tests.
+ * 
+ * By default, tests annotated with @DataJpaTest are transactional and roll back
+ * at the end of each test.
+ * They also use an embedded in-memory database
+ * (replacing any explicit or usually auto-configured DataSource).
+ * The @AutoConfigureTestDatabase annotation can be used to override these
+ * settings.
+ * 
+ * SQL queries are logged by default by setting the spring.jpa.show-sql property
+ * to true.
+ * This can be disabled using the showSql attribute.
+ * 
+ * If you are looking to load your full application configuration,
+ * but use an embedded database, you should consider @SpringBootTest
+ * combined with @AutoConfigureTestDatabase rather than this annotation.
  */
 @DataJpaTest
 public class OrganisationRepositoryTest {
@@ -26,5 +45,19 @@ public class OrganisationRepositoryTest {
     public void testFindAll() {
         List<OrganisationEntity> organisations = repository.findAll();
         assertTrue(organisations.isEmpty());
+    }
+
+    @Test
+    public void testsave() {
+        // given
+        OrganisationEntity organisation = new OrganisationEntity();
+        organisation.setName("myOrganisation");
+        // when
+        repository.save(organisation);
+        List<OrganisationEntity> organisations = repository.findByName("myOrganisation");
+        // then
+        assertTrue(!organisations.isEmpty());
+        assertEquals(1, organisations.size());
+        assertEquals(42L, organisation.getLonglongNumber());
     }
 }
